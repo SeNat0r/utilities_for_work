f0e3a3c4775e7cc3b22390de851fe009ad7a26dd
balabanovsb <sergey1993@yandex.ru>
2017-03-17 12:13:04 +0300

    +1

--- Thin_machine_server/main.py
+++ Thin_machine_server/main.py
@@ -36,17 +36,24 @@ from subprocess import call
 
 # Создание соединения
 class Connection(object):
-    def __init__(self, conn):
+    def __init__(self, conn, data, port):
         self.conn = conn
         self.sock = socket.socket()
+        self.data = data
+        self.port = port
 
     # Слушаем сокет
-    def open_connection(self):
-        self.sock.bind(('', 9595))
+    def listen(self):
+        self.sock.bind(('', self.port))
         self.sock(2)
         conn, addr = self.sock.accept()
         data = conn.recv(1024).decode()
         return data
+
+    # Отправка данных
+    def send(self, addr, data):
+        with self.sock.connect((addr, self.port)):
+            self.sock.send(data.encode())
 
 
 # Работа с конфигом
--- VM_client/main.py
+++ VM_client/main.py
@@ -2,38 +2,68 @@ import socket
 import configparser
 
 
-def connect(addr):
-    cmd = 'aaa'
-    sock = socket.socket()
-    sock.connect((addr, 9595))
-    sock.send(cmd.encode())
-    sock.close()
+# def connect(addr):
+#     cmd = 'aaa'
+#     sock = socket.socket()
+#     sock.connect((addr, 9595))
+#     sock.send(cmd.encode())
+#     sock.close()
+#
+#
+# def manager_edit(addr):
+#     config = configparser.RawConfigParser()
+#     config.read('default.ini')
+#     config.set('DEFAULT', 'manager', addr)
+#     with open('default.ini', 'w') as configfile:
+#         config.write(configfile)
+#
+#
+# def tm_edit(addr):
+#     config = configparser.RawConfigParser()
+#     config.read('default.ini')
+#     config.set('DEFAULT', 'thin_client', addr)
+#     with open('default.ini', 'w') as configfile:
+#         config.write(configfile)
+#
+#
+# def config_init():
+#     config = configparser.RawConfigParser()
+#     config.read('default.ini')
+#     manager = config.get('DEFAULT', 'manager')
+#     tm = config.get('DEFAULT', 'thin_client')
+#     return manager, tm
+#
+#
+# manager, thin = config_init()
+#
+# connect(thin)
 
+# Создание соединения
+class Connection(object):
+    def __init__(self, conn, data, port):
+        self.conn = conn
+        self.sock = socket.socket()
+        self.data = data
+        self.port = port
 
-def manager_edit(addr):
-    config = configparser.RawConfigParser()
-    config.read('default.ini')
-    config.set('DEFAULT', 'manager', addr)
-    with open('default.ini', 'w') as configfile:
-        config.write(configfile)
+    # Слушаем сокет
+    def listen(self):
+        self.sock.bind(('', self.port))
+        self.sock(2)
+        conn, addr = self.sock.accept()
+        data = conn.recv(1024).decode()
+        return data
 
+    # Отправка данных
+    def send(self, addr, data):
+        with self.sock.connect((addr, self.port)):
+            self.sock.send(data.encode())
 
-def tm_edit(addr):
-    config = configparser.RawConfigParser()
-    config.read('default.ini')
-    config.set('DEFAULT', 'thin_client', addr)
-    with open('default.ini', 'w') as configfile:
-        config.write(configfile)
+# Работа с конфигом
+class Config(object):
+    def __init__(self, addr):
+        self.config = configparser.RawConfigParser()
+        self.addr = addr
 
-
-def config_init():
-    config = configparser.RawConfigParser()
-    config.read('default.ini')
-    manager = config.get('DEFAULT', 'manager')
-    tm = config.get('DEFAULT', 'thin_client')
-    return manager, tm
-
-
-manager, thin = config_init()
-
-connect(thin)
+    def init_config(self):
+        pass
