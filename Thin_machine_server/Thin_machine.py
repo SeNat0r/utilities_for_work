e04867a6bfaa87e858ae5cccbd0f7cf94336761b
balabanovsb <sergey1993@yandex.ru>
2017-03-17 11:13:05 +0300

    add Connection class

--- Thin_machine_server/Thin_machine.py
+++ Thin_machine_server/Thin_machine.py
@@ -3,33 +3,47 @@ import socket
 from subprocess import call
 
 
-def test():
-    call('rundll32.exe user32.dll,LockWorkStation', shell=False)
+# def test():
+#     call('rundll32.exe user32.dll,LockWorkStation', shell=False)
+#
+#
+# def conn():
+#     sock = socket.socket()
+#     sock.bind(('', 9595))
+#     sock.listen(2)
+#     conn, addr = sock.accept()
+#     data = conn.recv(1024).decode()
+#     return data
+#
+#
+# def manager_edit(addr):
+#     config = configparser.RawConfigParser()
+#     config.read('default.ini')
+#     config.set('DEFAULT', 'manager', addr)
+#     with open('default.ini', 'w') as configfile:
+#         config.write(configfile)
+#
+#
+# def config_init():
+#     config = configparser.RawConfigParser()
+#     config.read('default.ini')
+#     manager = config.get('DEFAULT', 'manager')
+#     return manager
+#
+# while True:
+#     a = conn()
+#     test()
 
+class Connection(object):
+    def __init__(self, conn):
+        self.conn = conn
+        self.sock = socket.socket()
+        self.config = configparser.RawConfigParser()
 
-def conn():
-    sock = socket.socket()
-    sock.bind(('', 9595))
-    sock.listen(2)
-    conn, addr = sock.accept()
-    data = conn.recv(1024).decode()
-    return data
+    def open_connection(self):
+        self.sock.bind(('', 9595))
+        self.sock(2)
+        conn, addr = self.sock.accept()
+        data = conn.recv(1024).decode()
+        return data
 
-
-def manager_edit(addr):
-    config = configparser.RawConfigParser()
-    config.read('default.ini')
-    config.set('DEFAULT', 'manager', addr)
-    with open('default.ini', 'w') as configfile:
-        config.write(configfile)
-
-
-def config_init():
-    config = configparser.RawConfigParser()
-    config.read('default.ini')
-    manager = config.get('DEFAULT', 'manager')
-    return manager
-
-while True:
-    a = conn()
-    test()
