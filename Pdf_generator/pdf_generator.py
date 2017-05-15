37a12442d0500c178fb8f52e1ca3ce43ab571942
balabanovsb <sergey1993@yandex.ru>
2017-05-15 16:02:25 +0300

    +1

--- Pdf_generator/pdf_generator.py
+++ Pdf_generator/pdf_generator.py
@@ -6,7 +6,7 @@ from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.lib.pagesizes import A4
 from PyQt5.QtWidgets import (
     QApplication, QMainWindow, QWidget, QVBoxLayout, QFormLayout, QLabel, QLineEdit, QPushButton,
-    QComboBox, QSpacerItem, QSizePolicy
+    QComboBox, QSpacerItem, QSizePolicy, QRadioButton, QCheckBox
 )
 
 
@@ -25,16 +25,22 @@ class pdf_generator(QMainWindow):
         self.__lastName = QLabel('Фамилия:', self)
         self.__firstName = QLabel('Имя:', self)
         self.__citizenship = QLabel('Гражданство:', self)
-        self.__birthday = QLabel('Дата рождения: (Заполнять в формате: ДДММГГГГ)', self)
+        self.__birthday = QLabel('Дата рождения:\n(ДДММГГГГ)\n', self)
+        self.__sex = QLabel('Пол:', self)
+        self.__pob = QLabel('Место рождения:', self)
+        self.__state = QLabel('Государство:', self)
 
         self.__lastNameEdit = QLineEdit(self, maxLength=35)
         self.__firstNameEdit = QLineEdit(self, maxLength=35)
-
-        self.__citizenshipSelect = QComboBox(self)
-        citizenships = ['Беларусь']
-        self.__citizenshipSelect.addItems(citizenships)
-
-        self.__birthdayEdit = QLineEdit(self, maxLength=8)
+        self.__citizenshipEdit = QLineEdit(self, maxLength=35)
+        # self.__citizenshipSelect = QComboBox(self)
+        # citizenships = ['Беларусь']
+        # self.__citizenshipSelect.addItems(citizenships)
+        self.__birthdayEdit = QLineEdit(self, maxLength=10)
+        self.__maleRadio = QRadioButton('Мужской', self)
+        self.__femaleRadio = QRadioButton('Женский', self)
+        self.__stateCheck = QCheckBox('Совпадает с гражданством', self)
+        self.__stateEdit = QLineEdit(self, maxLength=35)
 
 
         self.__generate = QPushButton('Генерировать', self)
@@ -47,15 +53,28 @@ class pdf_generator(QMainWindow):
 
         self.mainLayout = QVBoxLayout(w)
         self.inputLayout = QFormLayout(w)
+        self.inputLayout2 = QFormLayout(w)
+        self.inputLayout3 = QFormLayout(w)
 
-        self.mainLayout.addWidget(self.__lastName)
-        self.mainLayout.addWidget(self.__lastNameEdit)
-        self.mainLayout.addWidget(self.__firstName)
-        self.mainLayout.addWidget(self.__firstNameEdit)
         self.mainLayout.addLayout(self.inputLayout)
-        self.inputLayout.addRow(self.__citizenship, self.__citizenshipSelect)
-        self.mainLayout.addWidget(self.__birthday)
-        self.mainLayout.addWidget(self.__birthdayEdit)
+        # self.mainLayout.addWidget(self.__lastName)
+        # self.mainLayout.addWidget(self.__lastNameEdit)
+        self.inputLayout.addRow(self.__lastName, self.__lastNameEdit)
+        # self.mainLayout.addWidget(self.__firstName)
+        # self.mainLayout.addWidget(self.__firstNameEdit)
+        self.inputLayout.addRow(self.__firstName, self.__firstNameEdit)
+        # self.inputLayout.addRow(self.__citizenship, self.__citizenshipSelect)
+        self.inputLayout.addRow(self.__citizenship, self.__citizenshipEdit)
+        # self.mainLayout.addWidget(self.__birthday)
+        # self.mainLayout.addWidget(self.__birthdayEdit)
+        self.inputLayout.addRow(self.__birthday, self.__birthdayEdit)
+        self.mainLayout.addWidget(self.__sex)
+        self.mainLayout.addLayout(self.inputLayout2)
+        self.inputLayout2.addRow(self.__maleRadio, self.__femaleRadio)
+        self.mainLayout.addWidget(self.__pob)
+        self.mainLayout.addWidget(self.__stateCheck)
+        self.mainLayout.addLayout(self.inputLayout3)
+        self.inputLayout3.addRow(self.__state, self.__stateEdit)
         self.mainLayout.addItem(self.spacer_2)
         self.mainLayout.addWidget(self.__generate)
 
@@ -69,8 +88,13 @@ class pdf_generator(QMainWindow):
 
         last_name = self.__lastNameEdit.text().upper()
         first_name = self.__firstNameEdit.text().upper()
-        citizenship = self.__citizenshipSelect.currentText().upper()
-        birthday = self.__birthdayEdit.text()
+        # citizenship = self.__citizenshipSelect.currentText().upper()
+        citizenship = self.__citizenshipEdit.text().upper()
+        birthday = ''
+        tempnum = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+        for symb in self.__birthdayEdit.text():
+            if symb in tempnum:
+                birthday += symb
 
         x = 85.5
         y = 697
@@ -115,6 +139,15 @@ class pdf_generator(QMainWindow):
             text.textLine(s)
             c.drawText(text)
             x += 13.3
+
+        if self.__maleRadio.isChecked():
+            text = c.beginText(351.8, y)
+            text.textLine('V')
+            c.drawText(text)
+        else:
+            text = c.beginText(418.3, y)
+            text.textLine('V')
+            c.drawText(text)
 
         c.save()
 
